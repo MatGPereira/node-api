8dc702ee40aa742bfcb8e1f2b2a442b4f4121e36
Matheus Pereira <matheusg.pereira89@outlook.com>
2024-02-11 16:12:41 -0300

    feat: Add middleware to convert request in json
    To work in the best way with the request (which is a NOde Stream), it was decided to create a middleware that will convert all the information received and place this information in the body of the request

---
+++ src/middlewares/to-json.js
@@ -0,0 +1,19 @@
+import { Buffer } from 'node:buffer';
+
+async function toJson(req, res) {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  res.setHeader('Content-Type', 'Application/json');
+}
+
+export { toJson };
