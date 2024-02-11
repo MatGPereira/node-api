62002b42df6ae375d324683638c10fc0471a1e69
Matheus Pereira <matheusg.pereira89@outlook.com>
2024-02-11 16:09:33 -0300

    feat: Add Database class
    Have access to database manipulation methods available for the application to perform basic CRUD operations

---
+++ src/database/database.js
@@ -0,0 +1,33 @@
+import fs from 'node:fs/promises';
+
+import { DATABASE_PATH } from '../env.js';
+
+class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(DATABASE_PATH, 'utf-8')
+      .then(data => (this.#database = JSON.parse(data)))
+      .catch(() => this.#persist());
+  }
+
+  #persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));
+  }
+
+  insert(table, data) {
+    const doesTableExists = Array.isArray(this.#database[table]);
+
+    if (doesTableExists) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+
+    return data;
+  }
+}
+
+export { Database };
